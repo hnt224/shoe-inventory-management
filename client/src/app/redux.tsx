affd8f8b545f769750d8fd15510da3a56e664de4
Hien Thi <hient.WBT@gmail.com>
2024-11-13 17:10:20 -0500

    edit

--- client/src/app/redux.tsx
+++ client/src/app/redux.tsx
@@ -26,16 +26,13 @@ import createWebStorage from "redux-persist/lib/storage/createWebStorage";
 /* REDUX PERSISTENCE */
 const createNoopStorage = () => {
   return {
-    getItem(_key: string) {
-      // Type `_key` as `string`
+    getItem(_key: any) {
       return Promise.resolve(null);
     },
-    setItem(_key: string, value: unknown) {
-      // Type `_key` as `string` and `value` as `unknown`
+    setItem(_key: any, value: any) {
       return Promise.resolve(value);
     },
-    removeItem(_key: string) {
-      // Type `_key` as `string`
+    removeItem(_key: any) {
       return Promise.resolve();
     },
   };
